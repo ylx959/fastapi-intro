db138602abbee22edd04c12e74790d0736b51068
ylx <a9634263@gmail.com>
2026-09-17 00:56:30 +0800

    save

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ app=FastAPI() #FastAPI 物件
 
 #非靜態檔案處理的路由，處在上方
 @app.get("/square")
-def square(num:Annotated[int,None]):
-    result=num*num
+def square(number:Annotated[int,Query(gt=0,lt=100)]):
+    result=number*number
     return {"data":result}
+
+@app.get("/multiply")
+def multiply(n1:Annotated[int,None],n2:Annotated[int,None]):
+    result=n1*n2
+    return {"result":result}
 
 @app.get("/member")
 def member():
